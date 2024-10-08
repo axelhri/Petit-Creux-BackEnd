bd28d4227b6dd746bbac384126c6c28f09089d06
axelhri <axelhouairi@gmail.com>
2024-10-08 16:27:53 +0200

    auth done

--- src/app.js
+++ src/app.js
@@ -1,14 +1,42 @@
-import express from "express";
+import "express-async-errors";
 import "dotenv/config";
+import express from "express";
 const app = express();
-import notFound from "./middlewares/not-found.middleware.js";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 import errorHandler from "./middlewares/error-handler.js";
+import notFound from "./middlewares/not-found.middleware.js";
+import connectDB from "./config/db.config.js";
+import mongooseSanitize from "express-mongo-sanitize";
+
+connectDB();
+
+app.use(helmet());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // store: ... , // Redis, Memcached, etc. See below.
+  })
+);
+app.set("trust proxy", 1);
+app.use(mongooseSanitize());
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(StatusCodes.OK).send("Hello World!");
+  res
+    .status(StatusCodes.OK)
+    .send("<h1>API JOBS</h1><a href='/api-docs'>Documentation</a>");
 });
 
+import { auth } from "./features/auth/index.js";
 import { StatusCodes } from "http-status-codes";
+
+app.use("/api/v1/auth", auth);
 
 app.use(notFound);
 app.use(errorHandler);
